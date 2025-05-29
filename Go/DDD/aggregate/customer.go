d457a81a8e2c3bb13cb13b9439397fb32e190662
sunimalherath <sunimaldv@gmail.com>
2025-05-29 22:47:13 +1000

    implementing NewCustomer

--- Go/DDD/aggregate/customer.go
+++ Go/DDD/aggregate/customer.go
@@ -1,13 +1,39 @@
 package aggregate
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
 	"github.com/sunimalherath/construct/ddd/entity"
 	"github.com/sunimalherath/construct/ddd/valueobject"
+)
+
+var (
+	ErrInvalidPerson = errors.New("invalid person")
 )
 
 type Customer struct {
 	person   *entity.Person // Person is the root entity of Customer, hence person.ID will be the main identifier for the customer.
 	products []*entity.Item
 
-	transactions valueobject.Transaction
+	transactions []valueobject.Transaction
 }
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		ID:   uuid.New(),
+		Name: name,
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
+
+// pointer types used here because the Entities can change state.
