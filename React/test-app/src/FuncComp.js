f967511b437a38811a2979c73716994ee5703853
sunimalherath <sunimaldv@gmail.com>
2025-06-07 23:39:09 +1000

    adding functional component

---
+++ React/test-app/src/FuncComp.js
@@ -0,0 +1,13 @@
+// A Functional Component
+import React, { Fragment } from "react";
+
+function TestComponent() {
+    return (
+        <Fragment>
+            <hr/>
+            <h1>This is a functional Component</h1>
+        </Fragment>
+    )
+}
+
+export default TestComponent;
