c655aabbd62f6c97e2a23a0b11da0abf9373e77e
sunimalherath <sunimaldv@gmail.com>
2025-06-07 23:25:49 +1000

    implementing class component

---
+++ React/test-app/src/AppClass.js
@@ -0,0 +1,10 @@
+// A Class Component
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+    render() {
+        return (
+            <h1>This is a class Component</h1>
+        );
+    }
+}
