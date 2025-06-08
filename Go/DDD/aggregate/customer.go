b151fab59554920a49bde40f9f01c86312da5e7c
Sunimal Herath <sunimal@live.com>
2025-06-08 17:11:42 +1000

    adding comments to customer

--- Go/DDD/aggregate/customer.go
+++ Go/DDD/aggregate/customer.go
@@ -12,11 +12,19 @@ var (
 	ErrInvalidPerson = errors.New("invalid person")
 )
 
+/*
+Customer fields are not accessible from outside the package - Why? Aggregates should not be directly accessible to access the data.
+It's not up to the aggregate how the data is formatted, ==>it's up to the value object.
+both person and product are entities and hence are mutable - that's why the pointer types are used.
+This reflects the changes here if the person or products are changed elsewhere.
+transactions are value objects (immutable) and hence don't use pointers here.
+*/
+
 type Customer struct {
 	person   *entity.Person // Person is the root entity of Customer, hence person.ID will be the main identifier for the customer.
-	products []*entity.Item
+	products []*entity.Item // Customer can buy many products
 
-	transactions []valueobject.Transaction
+	transactions []valueobject.Transaction // Customer can have many transactions
 }
 
 // NewCustomer is a factory to create a new customer.
