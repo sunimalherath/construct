c2dc9a72afee0fa779e3ab7db9a392fbbb1aa9e9
suni.o <sunimaldv@gmail.com>
2025-10-11 16:38:22 +1100

    adding port func and echo handler

--- Go/Cloud-Native-Go/microservice.go
+++ Go/Cloud-Native-Go/microservice.go
@@ -1,18 +1,36 @@
 package main
 
-
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/echo", echo)
+	http.ListenAndServe(port(), nil)
 }
-
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello Cloud Native Go")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+
+	w.Header().Add("Content-Type", "text/plain")
+
+	fmt.Fprintf(w, message)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	return fmt.Sprintf(":%s", port)
 }
