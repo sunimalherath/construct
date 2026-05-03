7ac9699fa6e778622e65d3ea24ce4cf9d53a26b4
suni.o <sunimaldv@gmail.com>
2026-05-03 11:15:57 +1000

    adding props

--- React/pizza-menu/src/index.js
+++ React/pizza-menu/src/index.js
@@ -71,13 +71,32 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        photoName="pizzas/spinaci.jpg"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        photoName="pizzas/funghi.jpg"
+        ingredients="Fungus, mozarella, and some other cheese"
+        price={15}
+      />
     </main>
   );
 }
 
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price + 2}</span>
+    </div>
+  );
+}
 function Footer() {
   const hour = new Date().getHours();
 
@@ -96,16 +115,6 @@ function Footer() {
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Spinaci pizza" />
-      <h3>Pizza</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
