8b51357ff31faf1d5ef63e63007c1512cbbaf189
suni.o <sunimaldv@gmail.com>
2026-05-07 22:15:46 +1000

    conditional rendering with &&

--- React/pizza-menu/src/index.js
+++ React/pizza-menu/src/index.js
@@ -68,14 +68,18 @@ function Header() {
 }
 
 function Menu() {
+  const numPizzas = pizzaData.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} />
+          ))}
+        </ul>
+      )}
       {/* <Pizza
         name="Pizza Spinaci"
         photoName="pizzas/spinaci.jpg"
@@ -111,7 +115,13 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleDateString()} We're currently open.
+      {/* {new Date().toLocaleDateString()} We're currently open. */}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
