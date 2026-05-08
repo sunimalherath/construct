022bd0e765f47069b9161a18a4598be882893c79
suni.o <sunimaldv@gmail.com>
2026-05-08 23:06:26 +1000

    conditional rendering with ternary operator

--- React/pizza-menu/src/index.js
+++ React/pizza-menu/src/index.js
@@ -68,17 +68,22 @@ function Header() {
 }
 
 function Menu() {
-  const numPizzas = pizzaData.length;
+  // const pizzaList = pizzaData;
+  const pizzaList = [];
+  // const numPizzas = pizzaData.length;
+  const numPizzas = pizzaList.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
-          {pizzaData.map((pizza) => (
+          {pizzaList.map((pizza) => (
             <Pizza pizzaObj={pizza} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
       {/* <Pizza
         name="Pizza Spinaci"
@@ -116,11 +121,15 @@ function Footer() {
   return (
     <footer className="footer">
       {/* {new Date().toLocaleDateString()} We're currently open. */}
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to have you between {openHour}:00 and {closeHour}:00.{" "}
+        </p>
       )}
     </footer>
   );
