3d20fd8ba2eeecff373b7ee047edc1c547579300
Sunimal Herath <sunimaldv@gmail.com>
2026-05-19 23:34:37 +1000

    initial commit

---
+++ Rust/variables/src/main.rs
@@ -0,0 +1,14 @@
+
+const STATING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let mut missiles = STATING_MISSILES;
+    let ready = READY_AMOUNT;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
+
+    println!("{} missiles left", missiles);
+}
