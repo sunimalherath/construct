878ad7ceddfd854a618677151836052fad5d0aa6
Sunimal Herath <sunimaldv@gmail.com>
2026-05-24 23:37:00 +1000

    control flow changes

--- Rust/variables/src/main.rs
+++ Rust/variables/src/main.rs
@@ -3,8 +3,9 @@ const STATING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles = STATING_MISSILES;
-    let ready = READY_AMOUNT;
+    let (mut missiles, ready) = (STATING_MISSILES, READY_AMOUNT);
+    // let mut missiles = STATING_MISSILES;
+    // let ready = READY_AMOUNT;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
 
