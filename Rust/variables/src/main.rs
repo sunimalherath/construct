331fd2d079eeea885807099c6ac9f07b927cb8cf
suni.o <sunimaldv@gmail.com>
2026-05-31 23:49:34 +1000

    adding differet ways to deal with variables

--- Rust/variables/src/main.rs
+++ Rust/variables/src/main.rs
@@ -3,13 +3,16 @@ const STATING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let (mut missiles, ready) = (STATING_MISSILES, READY_AMOUNT);
+    // let (mut missiles, ready) = (STATING_MISSILES, READY_AMOUNT);
+    let (missiles, ready) = (STATING_MISSILES, READY_AMOUNT);
     // let mut missiles = STATING_MISSILES;
     // let ready = READY_AMOUNT;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles = missiles - ready;
+    // missiles = missiles - ready;
+    // missiles -= ready;
 
-    println!("{} missiles left", missiles);
+    // println!("{} missiles left", missiles);
+    println!("{} missiles left", missiles - ready);
 }
