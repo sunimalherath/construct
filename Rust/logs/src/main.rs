fcd10e16c95e4527a0c39f96a5f960f7693bb659
Sunimal Herath <sunimaldv@gmail.com>
2026-08-09 00:18:14 +1000

    extracting error lines from logs

---
+++ Rust/logs/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+fn extract_errors(text: &str) -> Vec<&str> {
+    let all_text = text.split("\n");
+
+    let mut err_text = vec![];
+
+    for line in all_text {
+        if line.starts_with("ERROR") {
+            err_text.push(line);
+        }
+    }
+
+    err_text
+}
+
+fn main() {
+    match fs::read_to_string("logs.txt") {
+        Ok(text) => {
+            println!("error text {:#?}", extract_errors(text.as_str()));
+        }
+        Err(msg) => {
+            println!("{}", msg);
+        }
+    }
+}
