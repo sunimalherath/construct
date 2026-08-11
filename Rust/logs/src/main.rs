9c7b45cb008a02ec470164cc7e1f3afdc3f66382
sunimalherath <sunimaldv@gmail.com>
2026-08-12 00:41:53 +1000

    commenting the previous code for clarity

--- Rust/logs/src/main.rs
+++ Rust/logs/src/main.rs
@@ -20,17 +20,17 @@ fn main() {
     let error_log = extract_errors(text.as_str());
     fs::write("errors.txt", error_log.join("/n")).expect("failed to write to the errors file");
 
-    match fs::read_to_string("logs.txt") {
-        Ok(text) => {
-            let error_log = extract_errors(text.as_str());
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text) => {
+    //         let error_log = extract_errors(text.as_str());
 
-            match fs::write("errors.txt", error_log.join("\n")) {
-                Ok(..) => println!("wrote to errors.txt"),
-                Err(msg) => println!("failed to write to errors.txt due to: {}", msg),
-            }
-        }
-        Err(msg) => {
-            println!("{}", msg);
-        }
-    }
+    //         match fs::write("errors.txt", error_log.join("\n")) {
+    //             Ok(..) => println!("wrote to errors.txt"),
+    //             Err(msg) => println!("failed to write to errors.txt due to: {}", msg),
+    //         }
+    //     }
+    //     Err(msg) => {
+    //         println!("{}", msg);
+    //     }
+    // }
 }
