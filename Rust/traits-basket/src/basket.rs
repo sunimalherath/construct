eaecb2c7a0f3d94ee5dab120017d78f3661e3847
Suni H <sunimaldv@gmail.com>
2026-08-31 00:07:00 +1000

    replacing string with generic type

--- Rust/traits-basket/src/basket.rs
+++ Rust/traits-basket/src/basket.rs
@@ -1,17 +1,17 @@
-pub struct Basket{
-    item: Option<String>,
+pub struct Basket<T> {
+    item: Option<T>,
 }
 
-impl Basket {
-    pub fn new(item: String) -> Self {
+impl<T> Basket<T> {
+    pub fn new(item: T) -> Self {
         Basket { item: Some(item) }
     }
 
-    pub fn get(&mut self) -> Option<String> {
+    pub fn get(&mut self) -> Option<T> {
         self.item.take()
     }
 
-    pub fn put(&mut self, item: String) {
+    pub fn put(&mut self, item: T) {
         self.item = Some(item)
     }
 
--- Rust/traits-basket/src/main.rs
+++ Rust/traits-basket/src/main.rs
@@ -5,5 +5,9 @@ use basket::Basket;
 fn main() {
     let b1 = Basket::new(String::from("first item"));
 
+    let b2 = Basket::new(10);
+
+    let b3 = Basket::new(true);
+
     println!("Hello, world!");
 }
