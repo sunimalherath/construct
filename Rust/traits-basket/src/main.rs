7fb683ef165e96e21b63f9da5760124c1d17f989
Suni H <sunimaldv@gmail.com>
2026-09-01 23:34:26 +1000

    making the stack generic

--- Rust/traits-basket/src/main.rs
+++ Rust/traits-basket/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let b3 = Basket::new(true);
 
     let s1 = Stack::new(vec![String::from("stack item")]);
+    let s2 = Stack::new(vec![1,2,3,4]);
 
     println!("Hello, world!");
 }
