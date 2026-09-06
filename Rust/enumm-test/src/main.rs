1944afe786ad6d18ff8e3a9445b71528f856578e
Suni H <sunimaldv@gmail.com>
2026-09-07 00:49:17 +1000

    implementing media enum for testing

---
+++ Rust/enumm-test/src/main.rs
@@ -0,0 +1,51 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String }
+}
+
+fn print_media(media: &Media) {
+    println!("{:#?}", media)
+}
+
+impl Media {
+    fn media_description(&self) -> String {
+        match self {
+            Media::Book { title, author } => {
+                format!("Book: {} - {}", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("Movie: {} by {}", title, director)
+            }
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
+        }
+    }
+    
+}
+
+fn main() {
+    let bk = Media::Book { 
+        title: String::from("Martian"), 
+        author: String::from("Andy W"),
+    };
+
+    print_media(&bk);
+
+    let mv = Media::Movie { 
+        title: String::from("Titanic") , 
+        director: String::from("James Cameron")
+    };
+
+    print_media(&mv);
+
+    let ab = Media::Audiobook { title: String::from("I am legand") };
+
+    print_media(&ab);
+
+    println!("{}", bk.media_description());
+    println!("{}",mv.media_description());
+    println!("{}", ab.media_description());
+}
